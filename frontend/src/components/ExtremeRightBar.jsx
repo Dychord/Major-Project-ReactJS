224f4ec1109b845eb75f7c2a2590deab90eb9f34
chaudharyvicky564@gmail.com <chaudharyvicky564@gmail.com>
2024-12-23 12:36:02 +0530

    base commit

--- frontend/src/components/ExtremeRightBar.jsx
+++ frontend/src/components/ExtremeRightBar.jsx
@@ -3,13 +3,7 @@ import { SlNote } from "react-icons/sl";
 import SearchBar from './comp/SearchBar';
 import MessagesCard from './comp/MessagesCard';
 
-
-
 function ExtremeRightBar() {
-
-    
-
-      
   return (
     <>
         <div className='relative mt-24 w-full flex flex-col items-center gap-3 h-[88%] rounded-lg overflow-hidden bg-zinc-800/60'>
@@ -22,8 +16,8 @@ function ExtremeRightBar() {
                 <div className=' w-[80%]'><SearchBar /></div>
                 <div className='w-[90%] flex items-center justify-between bold font-semibold mt-2'>
                     <div className='flex gap-5 cursor-pointer'>
-                        <h1 className='relative'>Primary <span className='w-full bg-zinc-200 h-px p-px absolute -bottom-[7px] left-0'></span></h1>
-                        <h1 className='text-zinc-500'>General</h1>
+                        <h1 className='relative'>Friends <span className='w-full bg-zinc-200 h-px p-px absolute -bottom-[7px] left-0'></span></h1>
+                        <h1 className='text-zinc-500'>Groups</h1>
                     </div>
                     <h1 className='text-blue-600 cursor-pointer'>Requests (8)</h1>
                 </div>
@@ -32,9 +26,6 @@ function ExtremeRightBar() {
             <div className='overflow-y-scroll'>
                 <div className='w-full flex flex-col flex-wrap gap-1 mt-4 mb-2'><MessagesCard /></div>
             </div>
-            
-
-
         </div>
     </>
   )
